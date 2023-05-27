b1dc358f5729687b7dc16fb4b89745b93d51956a
prthmgoyl <prthmgoyl@gmail.com>
2023-05-27 14:02:05 +0530

    Getting dataframe

--- StockPredictionModel.py
+++ StockPredictionModel.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib as mp
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -8,8 +8,10 @@ from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv('MARUTI.csv')
 
-print(df)
-
 print(df.head())
 
 print(df.shape)
+
+print(df.describe())
+
+print(df.info())
